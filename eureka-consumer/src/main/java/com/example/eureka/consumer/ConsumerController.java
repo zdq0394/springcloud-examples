6ac062cac758ce3613ac97e391e634fd06af6787
zdq0394 <zdq123.hn@163.com>
2018-12-29 17:47:32 +0800

    eureka producer

--- eureka-consumer/src/main/java/com/example/eureka/consumer/ConsumerController.java
+++ eureka-consumer/src/main/java/com/example/eureka/consumer/ConsumerController.java
@@ -1,4 +1,16 @@
 package com.example.eureka.consumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ConsumerController {
+    @Autowired
+    HelloRemote HelloRemote;
+    @RequestMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return HelloRemote.hello(name);
+    }
 }
--- eureka-producer/src/main/java/com/example/eureka/producer/HelloController.java
+++ eureka-producer/src/main/java/com/example/eureka/producer/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(@RequestParam String name) {
-        return "Hello"+name+", this is first message.";
+        return "Hello "+name+", this is first message.";
     }
 }
