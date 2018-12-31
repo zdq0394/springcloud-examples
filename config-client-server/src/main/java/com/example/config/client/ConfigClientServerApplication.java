63cfa03b4686a8b7c9786e5be18d0529143ab34d
zdq0394 <zdq123.hn@163.com>
2018-12-31 19:36:44 +0800

    config client

---
+++ config-client-server/src/main/java/com/example/config/client/ConfigClientServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientServerApplication.class, args);
+	}
+
+}
+
+
---
+++ config-client-server/src/main/java/com/example/config/client/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.config.client.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloController{
+    @Value("${orchid.hello}")
+    private String hello;
+    @RequestMapping("/hello")
+    public String from(){
+        return this.hello;
+    }
+}
