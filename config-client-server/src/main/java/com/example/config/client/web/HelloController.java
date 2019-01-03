cdade46f32f702ce10e6f5cf876282793b497278
zdq0394 <zdq123.hn@163.com>
2019-01-03 14:38:12 +0800

    ok updated

--- config-client-server/src/main/java/com/example/config/client/web/HelloController.java
+++ config-client-server/src/main/java/com/example/config/client/web/HelloController.java
@@ -1,10 +1,12 @@
 package com.example.config.client.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 class HelloController{
     @Value("${orchid.hello}")
     private String hello;
