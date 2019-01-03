d512979497eda60922134e3fce8275b1c89ce897
zdq0394 <zdq123.hn@163.com>
2019-01-03 17:00:31 +0800

    config with eureka

---
+++ config-client-eureka/src/main/java/com/example/config/client/eureka/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.config.client.eureka;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+class HelloController{
+    @Value("${orchid.hello}")
+    private String hello;
+    @RequestMapping("/hello")
+    public String from(){
+        return this.hello;
+    }
+}
---
+++ config-git-eureka/src/test/java/com/example/config/git/eureka/ConfigGitEurekaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.config.git.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigGitEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
