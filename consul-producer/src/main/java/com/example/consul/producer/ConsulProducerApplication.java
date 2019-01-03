3f6287bb56fb9a90e86b7c25a9fdf1e81dd445f5
zdq0394 <zdq123.hn@163.com>
2019-01-03 10:19:33 +0800

    remove unused annotation

--- consul-producer/src/main/java/com/example/consul/producer/ConsulProducerApplication.java
+++ consul-producer/src/main/java/com/example/consul/producer/ConsulProducerApplication.java
@@ -1,15 +1,11 @@
 package com.example.consul.producer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@Configuration
-@EnableAutoConfiguration
 public class ConsulProducerApplication {
 
 	public static void main(String[] args) {
