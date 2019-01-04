a2be7f127e9f2a49ca99821c8dd48374c2d96db2
zdq0394 <zdq123.hn@163.com>
2019-01-04 12:48:15 +0800

    custom zuul filter and zuul fallback provider

--- zuul-eureka/src/main/java/com/example/zuul/eureka/ZuulEurekaApplication.java
+++ zuul-eureka/src/main/java/com/example/zuul/eureka/ZuulEurekaApplication.java
@@ -1,9 +1,13 @@
 package com.example.zuul.eureka;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.mvc.condition.RequestCondition;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -13,5 +17,35 @@ public class ZuulEurekaApplication {
 		SpringApplication.run(ZuulEurekaApplication.class, args);
 	}
 
+	@Bean
+	public MyFilter getMyFilter() {
+		return new MyFilter();
+	}
+
 }
 
+class MyFilter extends ZuulFilter {
+	@Override
+	public String filterType(){
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder(){
+		return 0;
+	}
+
+	@Override
+	public boolean shouldFilter(){
+		return true;
+	}
+	@Override
+	public Object run(){
+		RequestContext ctx = RequestContext.getCurrentContext();
+		System.out.println("My Custom Filter");
+		ctx.setSendZuulResponse(true);
+		ctx.setResponseStatusCode(200);
+		ctx.set("isSuccess",true);
+		return null;
+	}
+}
---
+++ zuul-eureka/src/main/java/com/example/zuul/eureka/fallback/EurekaProducerFallback.java
@@ -0,0 +1,58 @@
+package com.example.zuul.eureka.fallback;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class EurekaProducerFallback implements FallbackProvider {
+    @Override
+    public String getRoute() {
+        return "spring-cloud-producer";
+    }
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            public void close() {
+
+            }
+
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("The service is unavailable.".getBytes());
+            }
+
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(Throwable cause) {
+        if (cause != null && cause.getCause() != null) {
+            String reason = cause.getCause().getMessage();
+            System.out.println(reason);
+        }
+        return fallbackResponse();
+    }
+}
