6e5b56e35e4c0fd0e574f11477a5ea2c8c382a7e
zdq0394 <zdq123.hn@163.com>
2019-01-08 18:15:28 +0800

    eureka-consumer

--- eureka-consumer/src/main/java/com/example/eureka/consumer/ConsumerController.java
+++ eureka-consumer/src/main/java/com/example/eureka/consumer/ConsumerController.java
@@ -3,14 +3,15 @@ package com.example.eureka.consumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConsumerController {
     @Autowired
     HelloRemote HelloRemote;
-    @RequestMapping("/hello/{name}")
-    public String index(@PathVariable("name") String name) {
+    @RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
+    public String hello(@PathVariable("name") String name) {
         return HelloRemote.hello(name);
     }
 }
--- eureka-consumer/src/main/java/com/example/eureka/consumer/HelloRemoteHystrix.java
+++ eureka-consumer/src/main/java/com/example/eureka/consumer/HelloRemoteHystrix.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
-public class HelloRemoteHystrix implements HelloRemote{
+public class HelloRemoteHystrix implements HelloRemote {
     @Override
     public String hello(@RequestParam(value = "name") String name) {
         return "Hello "+name+", this is message in callback.";
